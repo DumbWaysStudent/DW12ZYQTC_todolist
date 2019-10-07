558c796065f81c2ab7e5c970d1a8bf2f7adb6a20
Arfifa Rahman <arfifarahman509@gmail.com>
2019-10-07 08:31:42 +0700

    add 2.add_todo

--- app/components/note.js
+++ app/components/note.js
@@ -1,5 +1,15 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, } from 'react-native';
+import {
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+} from 'react-native';
+
+import {
+    Button,
+} from 'native-base';
+
 
 export default class Note extends Component {
 
@@ -27,27 +37,49 @@ export default class Note extends Component {
                     id: 4,
                     task: "run"
                 },
-            ]
+            ],
+            text: ''
         }
+    }
+
+    _handleAddButton = () => {
+        let newTaskName = this.state.text
+        let id = this.state.noteArray.length + 1
+        const inputBaru = { "id": id, "task": newTaskName }
+        this.setState({ noteArray: [...this.state.noteArray, inputBaru], text: '' })
     }
 
     render() {
         return (
+            <View style={styles.container}>
+                <View style={styles.inputContainer}>
+                    <TextInput
+                        style={styles.inputText}
+                        value={this.state.text}
+                        placeholder="Add new task"
+                        onChangeText={text => this.setState({ text })} />
 
-            <View>
-                {this.state.noteArray.map(list => {
-                    return (
-                        <View key={list.id} style={styles.listNote}>
-                            <Text style={styles.fontNote}>{list.task}</Text>
-                        </View>
-                    )
-                })}
+                    <Button onPress={() => this._handleAddButton()} style={styles.btnAdd}><Text>Add</Text></Button>
+                </View>
+
+                <View>
+                    {this.state.noteArray.map(list => {
+                        return (
+                            <View key={list.id} style={styles.listNote}>
+                                <Text style={styles.fontNote}>{list.task}</Text>
+                            </View>
+                        )
+                    })}
+                </View>
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
+    container: {
+        padding: 10
+    },
     listNote: {
         borderWidth: 1,
         alignItems: "flex-start",
@@ -56,6 +88,23 @@ const styles = StyleSheet.create({
     fontNote: {
         fontSize: 20,
         padding: 10
+    },
+    inputContainer: {
+        flexDirection: "row",
+        paddingBottom: 20,
+        justifyContent: "space-between"
+    },
+    inputText: {
+        width: "73%",
+        borderWidth: 1,
+        paddingRight: 10,
+        height: 40
+    },
+    btnAdd: {
+        width: "25%",
+        justifyContent: "center",
+        height: 40,
+        backgroundColor: "#a8ff41"
     }
 })
 
